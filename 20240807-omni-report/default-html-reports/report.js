e30d11fa45fc7a15afccf6da3c02eea3e69b4414
huseyintorun <85924424+huseyintorun@users.noreply.github.com>
2024-08-09 21:45:00 +0200

    20240807-omni-report

---
+++ 20240807-omni-report/default-html-reports/report.js
@@ -0,0 +1,456 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/smoke_tests.feature");
+formatter.feature({
+  "name": "Omni Page Smoke Test",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Check the Basis Features of Omni_hypnosis Website",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user on main page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.the_user_on_main_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "check the URL of website correct",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.checkTheURLOfWebsiteCorrect()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNIfinder accepted",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNIfinderAccepted()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on AboutOMNI button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnAboutOMNIButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User confirm that Page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.userConfirmThatPage()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNI Language toggle to choose german",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNILanguageToggleToChooseGerman()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user check that Lamguage changed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.userCheckThatLamguageChanged()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Check the availability of order Process for Switzerland",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user on main page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.the_user_on_main_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNIfinder accepted",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNIfinderAccepted()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNI Language toggle to choose german",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNILanguageToggleToChooseGerman()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on BEGINNE NOCH HEUTE starten",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClicksOnBEGINNENOCHHEUTEStarten()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user checks that Order Process page opens",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserChecksThatOrderProcessPageOpens()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on Jetzt Platz in der Schweiz sichern",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClicksOnJetztPlatzInDerSchweizSichern()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user fills the form and choose Switzerland as country",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserFillsTheFormAndChooseSwitzerlandAsCountry()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on apply coupon button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnApplyCouponButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enter the coupon in the place holder",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserEnterTheCouponInThePlaceHolder()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on apply",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnApply()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on complete order and check if order completed or not",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnCompleteOrderAndCheckIfOrderCompletedOrNot()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Check the availability of order Process for Deutschland",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user on main page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.the_user_on_main_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNIfinder accepted",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNIfinderAccepted()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNI Language toggle to choose german",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNILanguageToggleToChooseGerman()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the User choose Deutchland from Hover Over Menu",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserChooseDeutchlandFromHoverOverMenu()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on Jetzt Platz in Deutschland sichern",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClicksOnJetztPlatzInDeutschlandSichern()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user fills the form",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserFillsTheForm()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on apply coupon button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnApplyCouponButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enter the coupon in the place holder",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserEnterTheCouponInThePlaceHolder()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on apply",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnApply()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on complete order and check if order completed or not",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnCompleteOrderAndCheckIfOrderCompletedOrNot()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Check the availability of order Process for USA",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user on main page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.the_user_on_main_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNIfinder accepted",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNIfinderAccepted()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on OMNI Language toggle to choose german",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnOMNILanguageToggleToChooseGerman()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the User choose US from Hover Over Menu",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserChooseUSFromHoverOverMenu()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user clicks on Secure your spot in the US",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClicksOnSecureYourSpotInTheUS()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user fills the form and choose USA as country",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserFillsTheFormAndChooseUSAAsCountry()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on apply coupon button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnApplyCouponButton()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user enter the coupon in the place holder",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserEnterTheCouponInThePlaceHolder()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on apply",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnApply()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "the user click on complete order and check if order completed or not",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.omni.stepdefinitions.OmnistartStepDefs.theUserClickOnCompleteOrderAndCheckIfOrderCompletedOrNot()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
---
+++ 20240807-omni-report/test-classes/features/omnistart.feature
@@ -0,0 +1,32 @@
+#@Smoke
+Feature: Omni Hypnose All Click On Links
+
+  Scenario:	Check the availability of Overhead Menu Elements
+    Given the user on main page
+    When the user click on OMNIfinder accepted
+    And the user click on AboutOMNI button
+    Then the user click on OMNI Language toggle to choose german
+
+#@wip
+  Scenario: Check the availability of Overhead Menu Elements
+    Given the user on main page
+    And the user click on OMNIfinder accepted
+    And the user click on OMNI Language toggle to choose german
+    And the user clicks on BEGINNE NOCH HEUTE starten
+    When the user clicks on Jetzt Platz in der Schweiz sichern
+    And the user fills the form
+    And the user click on apply coupon button
+    And the user enter the coupon in the place holder
+    And the user click on apply
+   Then the user click on complete order
+
+
+ # @Smoke
+ # Assertions eklenecek.
+  #Java Faker class ekle
+
+  #2. ürün https://www.omnihypnosis.com/courses/omni-hypnosetherapeut-zertifizierung-intensiv-live-deutschland-oesterreich
+# 3. https://www.omnihypnosis.com/courses/omni-hypnotherapist-certification-intensive-live-usa
+
+ # @Regression
+    # Öncelik smoke zaman kalirsa bu
